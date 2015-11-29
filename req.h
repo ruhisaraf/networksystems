20bcd8913782716063af498ff2c9adc6cd556264
ruhisaraf <ruhi.saraf14@gmail.com>
2015-11-29 01:14:16 -0700

    Create req.h

---
+++ req.h
@@ -0,0 +1,17 @@
+/* $begin req.h */
+#ifndef __REQ_H__
+#define __REQ_H__
+
+struct Req{
+	char *method;
+	char *url;
+	float version;
+	int conn_state;
+	int status;
+};
+
+void init_req(struct Req *http_req);
+void free_req(struct Req *http_req);
+int parse_request( char *buffer, struct Req *http_req, struct config *conf);
+
+#endif /*__REQ_H_*/
